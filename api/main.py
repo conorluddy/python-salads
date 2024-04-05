48512f871c0dd403edf448dab1316184cea6750c
Conor Luddy <conorluddy@gmail.com>
2024-04-05 14:35:37 +0100

    CORS middleware

--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from constants.config import ALLOWED_CORS_ORIGINS
 from database.lifespan import lifespan
 from routes.ingredients import ingredients_router
 from routes.locations import locations_router
@@ -8,11 +10,19 @@ from routes.seed import seed_router
 from routes.staff import staff_router
 
 
-# API Server
-# Run this from the root directory with `uvicorn main:app --reload`
+# API Server-  Run with `uvicorn main:app --reload`
 app = FastAPI(lifespan=lifespan)
 
-# Routers - TODO: Move these to a separate location/function
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ALLOWED_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routers
 app.include_router(locations_router, prefix="/locations", tags=["Locations"])
 app.include_router(staff_router, prefix="/staff", tags=["Staff"])
 app.include_router(menus_router, prefix="/menus", tags=["Menus"])
