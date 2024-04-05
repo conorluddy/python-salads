7391a95ca6f7484f1a75131c18b911e7ed08d597
Conor Luddy <conorluddy@gmail.com>
2024-04-05 13:51:54 +0100

    Get locations

--- api/routes/locations.py
+++ api/routes/locations.py
@@ -1,13 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlmodel import Session, select
+from database.tables import Locations
+from database.lifespan import engine
 
 locations_router = APIRouter()
 
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
 @locations_router.get("/")
-def get_all():
-    return {"message": "Stub for get-all"}
+def get_all(session: Session = Depends(get_session)):
+    locations = session.exec(select(Locations)).all()
+    return locations
 
 
-@locations_router.get("/{id}")
-async def get_by_id(id):
-    return {"message": "Stub for get-by-id"}
+@locations_router.get("/{location_id}")
+def get_by_id(location_id: int, session: Session = Depends(get_session)):
+    location = session.get(Locations, location_id)
+    if not location:
+        raise HTTPException(status_code=404, detail="Location not found")
+    return location
