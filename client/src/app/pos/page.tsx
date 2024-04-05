c7bf92bc78f275e0b9fc256cd4141aa96465d505
Conor Luddy <conorluddy@gmail.com>
2024-04-05 16:45:07 +0100

    Little note for the POS page

--- client/src/app/pos/page.tsx
+++ client/src/app/pos/page.tsx
@@ -8,6 +8,11 @@ export default function PointOfSales() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Point of Sales
+      <small className="m-10">
+        This page would ideally have actions where you can click the recipes to
+        add them to the order, and let you manage the modifiers/allergens on
+        each item. We're also missing our prices in the api data.
+      </small>
       <RecipeList />
       <NavigationMenu />
     </main>
