ba2f135edf49eb5442d4ac9220005b651363c900
Conor Luddy <conorluddy@gmail.com>
2024-04-05 16:18:09 +0100

    Quick and dirty list of stocked ingredients

--- client/src/app/inventory/page.tsx
+++ client/src/app/inventory/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import IngredientsList from "@/components/ui/inventory-list";
 import NavigationMenu from "@/components/ui/navigation-menu";
 import useRequireAuth from "@/hooks/useAuth";
 
@@ -6,7 +7,8 @@ export default function Inventory() {
   useRequireAuth();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Inventory Management
+      Inventory
+      <IngredientsList />
       <NavigationMenu />
     </main>
   );
